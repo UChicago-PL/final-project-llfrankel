cc188e6fca2436f5d50f2cf86c99fa60f416a863
leo frankel <llfrankel@uchicago.edu>
2026-02-24 17:39:22 -0600

    IR encode/decode working properly

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,14 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+
 module Main where
 
+import Data.Aeson (decode, encode)
+import Data.ByteString.Lazy qualified as BL
+import IR
+
 main :: IO ()
-main = putStrLn "hello"
+main = do
+  raw <- BL.getContents
+  case decode raw :: Maybe AnalysisInput of
+    Nothing -> putStrLn "PARSE FAILED"
+    Just input -> BL.putStr (encode input)
--- src/IR.hs
+++ src/IR.hs
@@ -3,8 +3,8 @@
 module IR where
 
 import Data.Aeson
-import Data.Map (Map)
 import Data.Aeson.Types (Parser)
+import Data.Map (Map)
 
 data Access = Free | Bound
   deriving (Show, Eq)
@@ -92,21 +92,23 @@ instance FromJSON Access where
     "free" -> pure Free
     "bound" -> pure Bound
     _ -> fail ("unknownn access:" ++ show t)
+
 instance ToJSON Access where
   toJSON Free = String "free"
   toJSON Bound = String "bound"
-
 
 instance FromJSON Dimension where
   parseJSON = withObject "Dimension" $ \ob -> do
     n <- ob .: "name"
     a <- ob .: "access"
     pure (Dimension n a)
+
 instance ToJSON Dimension where
-  toJSON d = object
-    [ "name"   .= dimName d
-    , "access" .= dimAccess d
-    ]
+  toJSON d =
+    object
+      [ "name" .= dimName d,
+        "access" .= dimAccess d
+      ]
 
 instance FromJSON PrimitiveSpec where
   parseJSON = withObject "PrimitiveSpec" $ \ob -> do
@@ -115,31 +117,34 @@ instance FromJSON PrimitiveSpec where
     hw <- ob .: "hardwareRequired"
     st <- ob .: "addsState"
     pure (PrimitiveSpec n od hw st)
+
 instance ToJSON PrimitiveSpec where
-  toJSON p = object
-    [ "name" .= primName p,
-      "outputDomain" .= primOutputDomain p,
-      "hardwareRequired" .= primHardware p,
-      "addsState" .= primAddsState p
-    ]
+  toJSON p =
+    object
+      [ "name" .= primName p,
+        "outputDomain" .= primOutputDomain p,
+        "hardwareRequired" .= primHardware p,
+        "addsState" .= primAddsState p
+      ]
 
 instance FromJSON BackendSpec where
   parseJSON = withObject "BackendSpec" $ \ob -> do
-    i  <- ob .: "identifier"
+    i <- ob .: "identifier"
     hw <- ob .: "hardwareOwned"
     sk <- ob .: "outputSinks"
     cs <- ob .: "coordinateSpecs"
     ps <- ob .: "primitiveSpecs"
     pure (BackendSpec i hw sk cs ps)
-instance ToJSON BackendSpec where
-  toJSON b = object
-    [ "identifier" .= backendId b
-    , "hardwareOwned" .= backendHardware b
-    , "outputSinks" .= backendSinks b
-    , "coordinateSpecs" .= backendCoords b
-    , "primitiveSpecs" .= backendPrimitives b
-    ]
 
+instance ToJSON BackendSpec where
+  toJSON b =
+    object
+      [ "identifier" .= backendId b,
+        "hardwareOwned" .= backendHardware b,
+        "outputSinks" .= backendSinks b,
+        "coordinateSpecs" .= backendCoords b,
+        "primitiveSpecs" .= backendPrimitives b
+      ]
 
 instance FromJSON Strand where
   parseJSON = withObject "Strand" $ \ob -> do
@@ -147,25 +152,27 @@ instance FromJSON Strand where
     i <- ob .: "index"
     e <- ob .: "expr"
     pure (Strand n i e)
-instance ToJSON Strand where
-  toJSON s = object
-    [ "name" .= strandName s
-    , "index" .= strandIndex s
-    , "expr" .= strandExpr s
-    ]
 
+instance ToJSON Strand where
+  toJSON s =
+    object
+      [ "name" .= strandName s,
+        "index" .= strandIndex s,
+        "expr" .= strandExpr s
+      ]
 
 instance FromJSON Bundle where
   parseJSON = withObject "Bundle" $ \ob -> do
     n <- ob .: "name"
     s <- ob .: "strands"
     pure (Bundle n s)
-instance ToJSON Bundle where
-  toJSON b = object
-    [ "name"    .= bundleName b
-    , "strands" .= bundleStrands b
-    ]
 
+instance ToJSON Bundle where
+  toJSON b =
+    object
+      [ "name" .= bundleName b,
+        "strands" .= bundleStrands b
+      ]
 
 instance FromJSON Spindle where
   parseJSON = withObject "Spindle" $ \ob -> do
@@ -174,55 +181,56 @@ instance FromJSON Spindle where
     l <- ob .: "locals"
     r <- ob .: "returns"
     pure (Spindle n p l r)
-instance ToJSON Spindle where
-  toJSON s = object
-    [ "name" .= spindleName s
-    , "params" .= spindleParams s
-    , "locals" .= spindleLocals s
-    , "returns" .= spindleReturns s
-    ]
 
+instance ToJSON Spindle where
+  toJSON s =
+    object
+      [ "name" .= spindleName s,
+        "params" .= spindleParams s,
+        "locals" .= spindleLocals s,
+        "returns" .= spindleReturns s
+      ]
 
 instance FromJSON OrderEntry where
   parseJSON = withObject "OrderEntry" $ \ob -> do
     b <- ob .: "bundle"
     s <- ob .:? "strands"
     pure (OrderEntry b s)
+
 instance ToJSON OrderEntry where
-    toJSON e = object
-      [ "bundle" .= orderBundle e
-      , "strands" .= orderStrands e
-      ]
+  toJSON e = object $ ("bundle" .= orderBundle e) : maybe [] (\s -> ["strands" .= s]) (orderStrands e)
 
 instance FromJSON Program where
   parseJSON = withObject "Program" $ \ob -> do
-    b   <- ob .: "bundles"
-    sp  <- ob .: "spindles"
+    b <- ob .: "bundles"
+    sp <- ob .: "spindles"
     or' <- ob .: "order"
-    r   <- ob .:? "resources" .!= []
-    tr  <- ob .:? "textResources" .!= []
+    r <- ob .:? "resources" .!= []
+    tr <- ob .:? "textResources" .!= []
     pure (Program b sp or' r tr)
-instance ToJSON Program where
-  toJSON p = object
-    [ "bundles" .= progBundles p
-    , "spindles" .= progSpindles p
-    , "order" .= progOrder p
-    , "resources" .= progResources p
-    , "textResources" .= progTextRes p
-    ]
 
+instance ToJSON Program where
+  toJSON p =
+    object
+      [ "bundles" .= progBundles p,
+        "spindles" .= progSpindles p,
+        "order" .= progOrder p,
+        "resources" .= progResources p,
+        "textResources" .= progTextRes p
+      ]
 
 instance FromJSON AnalysisInput where
   parseJSON = withObject "AnalysisInput" $ \ob -> do
     p <- ob .: "program"
     b <- ob .: "backends"
     pure (AnalysisInput p b)
-instance ToJSON AnalysisInput where
-  toJSON a = object
-    [ "program" .= inputProgram a
-    , "backends" .= inputBackends a
-    ]
 
+instance ToJSON AnalysisInput where
+  toJSON a =
+    object
+      [ "program" .= inputProgram a,
+        "backends" .= inputBackends a
+      ]
 
 instance FromJSON Expr where
   parseJSON = withObject "Expr" $ \ob -> do
@@ -230,7 +238,7 @@ instance FromJSON Expr where
     case tag of
       "num" -> Num <$> ob .: "value"
       "param" -> Param <$> ob .: "name"
-      "index" -> Index <$>  ob .: "bundle" <*> ob .: "indexExpr"
+      "index" -> Index <$> ob .: "bundle" <*> ob .: "indexExpr"
       "binary" -> Binary <$> ob .: "op" <*> ob .: "left" <*> ob .: "right"
       "unary" -> Unary <$> ob .: "op" <*> ob .: "operand"
       "call" -> Call <$> ob .: "spindle" <*> ob .: "args"
@@ -239,17 +247,18 @@ instance FromJSON Expr where
       "remap" -> Remap <$> ob .: "base" <*> ob .: "substitutions"
       "cacheRead" -> CacheRead <$> ob .: "cacheId" <*> ob .: "tapIndex"
       _ -> fail ("unknown expr type:" ++ tag)
+
 instance ToJSON Expr where
   toJSON expr = case expr of
-    Num v -> tagged "num"       ["value" .= v]
-    Param n -> tagged "param"     ["name" .= n]
-    Index b e -> tagged "index"     ["bundle" .= b, "indexExpr" .= e]
-    Binary op l r  -> tagged "binary"    ["op" .= op, "left" .= l, "right" .= r]
-    Unary op e -> tagged "unary"     ["op" .= op, "operand" .= e]
-    Call s args -> tagged "call"      ["spindle" .= s, "args" .= args]
-    Builtin n args -> tagged "builtin"   ["name" .= n, "args" .= args]
-    Extract c i -> tagged "extract"   ["call" .= c, "index" .= i]
-    Remap b subs -> tagged "remap"     ["base" .= b, "substitutions" .= subs]
+    Num v -> tagged "num" ["value" .= v]
+    Param n -> tagged "param" ["name" .= n]
+    Index b e -> tagged "index" ["bundle" .= b, "indexExpr" .= e]
+    Binary op l r -> tagged "binary" ["op" .= op, "left" .= l, "right" .= r]
+    Unary op e -> tagged "unary" ["op" .= op, "operand" .= e]
+    Call s args -> tagged "call" ["spindle" .= s, "args" .= args]
+    Builtin n args -> tagged "builtin" ["name" .= n, "args" .= args]
+    Extract c i -> tagged "extract" ["call" .= c, "index" .= i]
+    Remap b subs -> tagged "remap" ["base" .= b, "substitutions" .= subs]
     CacheRead c t -> tagged "cacheRead" ["cacheId" .= c, "tapIndex" .= t]
     where
       tagged t fields = object (("type" .= (t :: String)) : fields)
