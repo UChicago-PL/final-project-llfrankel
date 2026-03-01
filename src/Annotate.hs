70deab5243ef418ca5313ce0d9e70139bdf8e701
leo frankel <llfrankel@uchicago.edu>
2026-03-01 15:44:54 -0600

    progress on depGraph and annotate

--- src/Annotate.hs
+++ src/Annotate.hs
@@ -1,33 +1,145 @@
-module Annotate where
+module Annotate
+  ( Annotation (..),
+    AnnotateEnv (..),
+    annotateProgram,
+  )
+where
 
+import Control.Monad.Reader
+import Control.Monad.State
+import Data.List (find, stripPrefix)
+import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Maybe (fromMaybe)
 import Data.Set (Set)
 import qualified Data.Set as Set
 import IR
 
 data Annotation = Annotation
-  { aDomain :: [Dimension],
+  { aDomain :: Map String Dimension,
     aHardware :: Set String,
     aStateful :: Bool
   }
   deriving (Show, Eq)
 
-emptyA :: Annotation
-emptyA = Annotation [] Set.empty False
+instance Semigroup Annotation where
+  a <> b =
+    Annotation
+      { aDomain = Map.unionWith pick (aDomain a) (aDomain b),
+        aHardware = aHardware a <> aHardware b,
+        aStateful = aStateful a || aStateful b
+      }
+    where
+      pick d1 d2
+        | dimAccess d1 == Bound || dimAccess d2 == Bound = d1 {dimAccess = Bound}
+        | otherwise = d1
 
-merge :: Annotation -> Annotation -> Annotation
-merge a b =
-  Annotation
-    { aDomain = mergeDomains (aDomain a) (aDomain b),
-      aHardware = Set.union (aHardware a) (aHardware b),
-      aStateful = aStateful a || aStateful b
-    }
+instance Monoid Annotation where
+  mempty = Annotation Map.empty Set.empty False
 
-mergeDomains :: [Dimension] -> [Dimension] -> [Dimension]
-mergeDomains as bs = Map.elems merged
-  where
-    toMap ds = Map.fromList [(dimName d, d) | d <- ds]
-    merged = Map.unionWith pickRestrict (toMap as) (toMap bs)
-    pickRestrict a b
-      | dimAccess a == Bound || dimAccess b == Bound = a {dimAccess = Bound}
-      | otherwise = a
+data AnnotateEnv = AnnotateEnv
+  { envBundles :: Map String Bundle,
+    envCoords :: Map String Dimension,
+    envPrimitives :: Map String PrimitiveSpec
+  }
+
+data AnnotateState = AnnotateState
+  { stMemo :: Map String Annotation,
+    stComputing :: Set String
+  }
+
+type Annotator a = ReaderT AnnotateEnv (State AnnotateState) a
+
+runAnnotation :: AnnotateEnv -> Annotator a -> a
+runAnnotation env m = evalState (runReaderT m env) (AnnotateState Map.empty Set.empty)
+
+memoized :: String -> Annotator Annotation -> Annotator Annotation
+memoized key compute = do
+  st <- get
+  case Map.lookup key (stMemo st) of
+    Just ann -> return ann
+    Nothing
+      | Set.member key (stComputing st) -> return $ mempty {aStateful = True}
+      | otherwise -> do
+          modify $ \s -> s {stComputing = Set.insert key (stComputing s)}
+          ann <- compute
+          modify $ \s ->
+            s {stMemo = Map.insert key ann (stMemo s), stComputing = Set.delete key (stComputing s)}
+          return ann
+
+makeKey :: String -> Expr -> String
+makeKey bundle (Param field) = bundle ++ "." ++ field
+makeKey bundle (Num n) = bundle ++ "." ++ show (floor n :: Int)
+makeKey bundle _ = bundle
+
+findStrand :: Expr -> [Strand] -> Maybe Strand
+findStrand (Param field) strands = find (\s -> strandName s == field) strands
+findStrand (Num n) strands = find (\s -> strandIndex s == floor n) strands
+findStrand _ _ = Nothing
+
+lookupCoord :: String -> AnnotateEnv -> Annotation
+lookupCoord name env = case Map.lookup name (envCoords env) of
+  Just dim -> mempty {aDomain = Map.singleton (dimName dim) dim}
+  Nothing -> mempty
+
+findBundleStrand :: String -> Expr -> AnnotateEnv -> Maybe Strand
+findBundleStrand b e env = do
+  bun <- Map.lookup b (envBundles env)
+  findStrand e (bundleStrands bun)
+
+annotateExpr :: Expr -> Annotator Annotation
+annotateExpr expr = case expr of
+  Num _ -> return mempty
+  Param name -> asks (lookupCoord name)
+  CacheRead _ _ -> return $ mempty {aStateful = True}
+  Unary _ e -> annotateExpr e
+  Extract e _ -> annotateExpr e
+  Binary _ l r -> (<>) <$> annotateExpr l <*> annotateExpr r
+  Call _ args -> mconcat <$> mapM annotateExpr args
+  Builtin name args -> annotateBuiltin name args
+  Index "me" (Param field) -> asks (lookupCoord field)
+  Index "me" _ -> return mempty
+  Index b e -> do
+    let key = makeKey b e
+    memoized key $ do
+      strand <- asks (findBundleStrand b e)
+      maybe (return mempty) (annotateExpr . strandExpr) strand
+  Remap base subs -> annotateRemap base subs
+
+annotateBuiltin :: String -> [Expr] -> Annotator Annotation
+annotateBuiltin name args = do
+  merged <- mconcat <$> mapM annotateExpr args
+  spec <- asks (Map.lookup name . envPrimitives)
+  return $ case spec of
+    Nothing -> merged
+    Just s ->
+      Annotation
+        { aDomain = case primOutputDomain s of
+            [] -> aDomain merged
+            ds -> Map.fromList [(dimName d, d) | d <- ds],
+          aHardware = aHardware merged <> Set.fromList (primHardware s),
+          aStateful = aStateful merged || primAddsState s
+        }
+
+annotateRemap :: Expr -> Map String Expr -> Annotator Annotation
+annotateRemap base subs = do
+  baseAnn <- annotateExpr base
+  subAnns <- mconcat <$> mapM annotateExpr (Map.elems subs)
+  let removedDims = map (fromMaybe <*> stripPrefix "me.") (Map.keys subs)
+      baseDomain = foldr Map.delete (aDomain baseAnn) removedDims
+  return $
+    Annotation
+      { aDomain = baseDomain <> aDomain subAnns,
+        aHardware = aHardware baseAnn <> aHardware subAnns,
+        aStateful = aStateful baseAnn || aStateful subAnns
+      }
+
+annotateProgram :: AnnotateEnv -> Map String Annotation
+annotateProgram env = runAnnotation env $ do
+  let keys =
+        [ (bName ++ "." ++ strandName s, strandExpr s)
+          | (bName, b) <- Map.toList (envBundles env),
+            s <- bundleStrands b
+        ]
+  mapM_ (\(key, expr) -> memoized key (annotateExpr expr)) keys
+  gets stMemo
--- src/DepGraph.hs
+++ src/DepGraph.hs
@@ -1,1 +1,61 @@
-module DepGraph where
+module DepGraph (buildGraph, topoSort, dependents, transDeps) where
+
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
+import IR
+
+bundleRefs :: Expr -> Set String
+bundleRefs expr = case expr of
+  Num _ -> mempty
+  Param _ -> mempty
+  CacheRead _ _ -> mempty
+  Index "me" e -> bundleRefs e
+  Index b e -> Set.insert b $ bundleRefs e
+  Binary _ l r -> bundleRefs l <> bundleRefs r
+  Unary _ e -> bundleRefs e
+  Call _ args -> foldMap bundleRefs args
+  Builtin _ args -> foldMap bundleRefs args
+  Extract e _ -> bundleRefs e
+  Remap b subs -> bundleRefs b <> foldMap bundleRefs (Map.elems subs)
+
+-- | Build a dependency graph from a program. Each bundle maps to the
+-- set of other bundles it references. Self-references are excluded.
+buildGraph :: Program -> Map String (Set String)
+buildGraph prog = Map.mapWithKey Set.delete raw
+  where
+    raw = Map.map refsForBundle $ progBundles prog
+    refsForBundle b = foldMap (bundleRefs . strandExpr) (bundleStrands b)
+
+topoSort :: Map String (Set String) -> Maybe [String]
+topoSort graph = reverse <$> visit (Map.keys graph) Set.empty Set.empty []
+  where
+    visit [] _ _ result = Just result
+    visit (n : ns) visited visiting result
+      | Set.member n visited = visit ns visited visiting result
+      | Set.member n visiting = Nothing
+      | otherwise = do
+          let deps = Map.findWithDefault Set.empty n graph
+              visiting' = Set.insert n visiting
+          result' <- visit (Set.toList deps) visited visiting' result
+          visit ns (Set.insert n visited) (Set.delete n visiting') (n : result')
+
+-- | Reverse the dependency graph: for each bundle, which bundles depend on it.
+dependents :: Map String (Set String) -> Map String (Set String)
+dependents graph =
+  Map.fromListWith
+    Set.union
+    [ (dep, Set.singleton name)
+      | (name, deps) <- Map.toList graph,
+        dep <- Set.toList deps
+    ]
+
+-- | All bundles reachable from a starting node, transitively.
+transDeps :: Map String (Set String) -> String -> Set String
+transDeps graph start = dfs Set.empty (Map.findWithDefault Set.empty start graph)
+  where
+    dfs = Set.foldl' visit
+    visit seen n
+      | Set.member n seen = seen
+      | otherwise = dfs (Set.insert n seen) (Map.findWithDefault Set.empty n graph)
