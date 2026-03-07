b6747f043b4fde9038a2f7cae2b9a2f8b30da0c6
leo frankel <llfrankel@uchicago.edu>
2026-03-07 11:56:49 -0600

    main.hs working with weft-export

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,13 @@
 
 module Main where
 
-import Data.Aeson (decode, encode)
+import Annotate
+import Data.Aeson (decode)
 import Data.ByteString.Lazy qualified as BL
+import Data.Map qualified as Map
+import DepGraph
 import IR
+import IRHelpers
 
 -- right now this just outputs the regular JSON so I can check if its right
 main :: IO ()
@@ -12,4 +16,14 @@ main = do
   raw <- BL.getContents
   case decode raw :: Maybe AnalysisInput of
     Nothing -> putStrLn "PARSE FAILED"
-    Just input -> BL.putStr (encode input)
+    Just input -> do
+      let prog = inputProgram input
+          backends = inputBackends input
+          coords = foldMap backendCoords backends
+          prims = foldMap backendPrimitives backends
+          env = AnnotateEnv (progBundles prog) coords prims
+          annotations = annotateProgram env
+          graph = buildGraph prog
+      putStrLn $ "Bundles: " ++ show (Map.keys (progBundles prog))
+      putStrLn $ "Graph: " ++ show graph
+      putStrLn $ "Annotations: " ++ show annotations
